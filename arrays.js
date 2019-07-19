b58f731cfa97c6755ea8c00ed5af7ce2119648ed
Matthew C <m23.cullen@gmail.com>
2019-07-19 01:26:39 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,1 +1,3 @@
-var ingredient1 = "bread", ingredient2 = "mild cheese", ingredient3 = "sharp cheese", ingredient4 = "butter", ingredient5 = "tomato", ingredient6 = "garlic";
+var chocolateBars = ["snickers","hundred grand","kitkat","skittles"]
+
+function
