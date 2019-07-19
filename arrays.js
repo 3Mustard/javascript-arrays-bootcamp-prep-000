7e7120f591ffff0ecaca2d1de5fe4d5440540d32
Matthew C <m23.cullen@gmail.com>
2019-07-19 01:34:04 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -6,8 +6,7 @@ function addElementToBeginningOfArray(array,element){
 }
 
 function destructivelyAddElementToBeginningOfArray(array,element){
-  array.unshift(element);
-  return array;
+  return array.unshift(element);
 }
 
 function addElementToEndOfArray(array,element){
@@ -15,3 +14,6 @@ function addElementToEndOfArray(array,element){
   return newArray;
 }
 
+function destructivelyAddElementToEndOfArray(array,element){
+ return array.push(element);
+}
