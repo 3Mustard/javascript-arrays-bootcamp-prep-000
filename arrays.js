0bb8c401535ad85a74fd11cf7f74e2cc6652c733
Matthew C <m23.cullen@gmail.com>
2019-07-19 01:31:37 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,3 +1,11 @@
-var chocolateBars = ["snickers","hundred grand","kitkat","skittles"]
+var chocolateBars = ["snickers","hundred grand","kitkat","skittles"];
 
-function addElementToBeginningOfArray(){}
+function addElementToBeginningOfArray(array,element){
+  const newArray = [element, ...array];
+  return newArray;
+}
+
+function destructivelyAddElementToBeginningOfArray(array,element){
+  array.unshift(element);
+  return array;
+}
